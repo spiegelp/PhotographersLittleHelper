22cbe5f1b149bdeeb4102cadabaef59d4b987ca9
Spiegel Philipp <philipp.spiegel@vrz.net>
2022-09-09 17:09:07 +0200

    PhotoService for handling photos from the disk

---
+++ dotnet/PhotographersLittleHelper.Core/Pipe/PhotoData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotographersLittleHelper.Core.Pipe
+{
+    public class PhotoData
+    {
+        public byte[] Data { get; set; }
+
+        public ImageFormat Format { get; set; }
+
+        public PhotoData() { }
+    }
+}
---
+++ dotnet/PhotographersLittleHelper.Core/Services/PhotoService.cs
@@ -0,0 +1,37 @@
+﻿using PhotographersLittleHelper.Core.Pipe;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotographersLittleHelper.Core.Services
+{
+    public class PhotoService
+    {
+        public PhotoService() { }
+
+        public async Task<PhotoData> ReadPhotoAsync(string filename)
+        {
+            ImageFormat format = GetImageFormatForFilename(filename);
+            byte[] data = await File.ReadAllBytesAsync(filename).ConfigureAwait(false);
+
+            return new() { Data = data, Format = format };
+        }
+
+        public ImageFormat GetImageFormatForFilename(string filename)
+        {
+            FileInfo file = new(filename);
+            string extension = file.Extension.ToLower();
+
+            return extension switch
+            {
+                ".jpg" or ".jpeg" => ImageFormat.Jpeg,
+                ".png" => ImageFormat.Png,
+                ".gif" => ImageFormat.Gif,
+                _ => throw new ArgumentException($"Extension of {file.Name} is not supported.", nameof(filename)),
+            };
+        }
+    }
+}
