0a7f4b9477dc4b592aff2ae5439e82e2c4b5c8e2
Spiegel Philipp <philipp.spiegel@vrz.net>
2022-09-09 15:16:27 +0200

    desktop app setup

---
+++ dotnet/PhotographersLittleHelper.App/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using PhotographersLittleHelper.App.GuiLayer;
+using PhotographersLittleHelper.App.GuiLayer.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace PhotographersLittleHelper.App
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs args)
+        {
+            base.OnStartup(args);
+
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+
+            WindowViewModel windowViewModel = new();
+            MainWindow mainWindow = new(windowViewModel);
+
+            mainWindow.Show();
+        }
+    }
+}
---
+++ dotnet/PhotographersLittleHelper.App/GuiLayer/ViewModel/WindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using NuniToolbox.Ui.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotographersLittleHelper.App.GuiLayer.ViewModel
+{
+    public class WindowViewModel : NuniToolbox.Ui.ViewModel.WindowViewModel
+    {
+        public WindowViewModel(ViewModelObject viewModel = null, bool disposeOldViewModel = true)
+            : base(viewModel, disposeOldViewModel)
+        {
+        }
+    }
+}
