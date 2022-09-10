14c5adbb26bbbbb2294b0dd408d89f30ca2694c1
Spiegel Philipp <philipp.spiegel@vrz.net>
2022-09-10 13:53:33 +0200

    improved performance for grayscale

--- dotnet/PhotographersLittleHelper.Core/Pipe/GrayscaleStep.cs
+++ dotnet/PhotographersLittleHelper.Core/Pipe/GrayscaleStep.cs
@@ -17,7 +17,9 @@ namespace PhotographersLittleHelper.Core.Pipe
         {
             using MemoryStream msInput = new(input.Data);
             using Bitmap image = new(msInput);
-            using Bitmap destImage = new(image.Width, image.Height);
+            using Bitmap convertedImage = ChangePixelFormat(image, PixelFormat.Format24bppRgb);
+
+            /*using Bitmap destImage = new(image.Width, image.Height);
 
             for (int x = 0; x < image.Width; x++)
             {
@@ -29,13 +31,48 @@ namespace PhotographersLittleHelper.Core.Pipe
 
                     destImage.SetPixel(x, y, Color.FromArgb((int)grayscale, (int)grayscale, (int)grayscale));
                 }
+            }*/
+
+            // manipulating the bytes directly is much faster than SetPixel()
+
+            Rectangle rectangle = new(0, 0, convertedImage.Width, convertedImage.Height);
+
+            BitmapData bmpData = convertedImage.LockBits(rectangle, ImageLockMode.ReadWrite, convertedImage.PixelFormat);
+
+            IntPtr ptr = bmpData.Scan0;
+
+            int bytes = Math.Abs(bmpData.Stride) * convertedImage.Height;
+            byte[] rgbValues = new byte[bytes];
+
+            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+
+            for (int index = 0; index < rgbValues.Length; index = index + 3)
+            {
+                byte grayscale = (byte)(0.3 * rgbValues[index + 2] + 0.59 * rgbValues[index + 1] + 0.11 * rgbValues[index]);
+
+                rgbValues[index] = grayscale;
+                rgbValues[index + 1] = grayscale;
+                rgbValues[index + 2] = grayscale;
             }
 
+            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+
+            convertedImage.UnlockBits(bmpData);
+
             using MemoryStream msOutput = new();
-            destImage.Save(msOutput, input.Format);
+            convertedImage.Save(msOutput, input.Format);
 
             return new() { Data = msOutput.ToArray(), Filename = input.Filename, Format = input.Format };
         }
 #pragma warning restore CS1998
+
+        private Bitmap ChangePixelFormat(Bitmap image, PixelFormat pixelFormat)
+        {
+            Bitmap destImage = new(image.Width, image.Height, pixelFormat);
+            using Graphics graphics = Graphics.FromImage(destImage);
+            graphics.DrawImage(image, new Rectangle(0, 0, image.Width, image.Height));
+
+            return destImage;
+        }
     }
 }
